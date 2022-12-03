1525f7aceadeff2972d2cca76bfb343cfbb49b92
Diablo-S <shreyashnegi123@gmail.com>
2022-12-03 15:52:00 +0530

    4th commit

--- Stylemart/frontend/src/App.js
+++ Stylemart/frontend/src/App.js
@@ -1,3 +1,6 @@
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
@@ -5,17 +8,27 @@ import ProductScreen from "./screens/ProductScreen";
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <a href="/">StyleMart</a>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>StyleMArt</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen/>}/>
-            <Route path="/" element={<HomeScreen/>}/>
-          </Routes>
-          
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All Rights Reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
