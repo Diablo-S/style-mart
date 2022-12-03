3a904eb607bde2f26b08145d9b47bd7a326562c8
Diablo-S <shreyashnegi123@gmail.com>
2022-12-03 23:34:06 +0530

    10th commit

--- Stylemart/frontend/src/screens/ProductScreen.js
+++ Stylemart/frontend/src/screens/ProductScreen.js
@@ -149,7 +149,7 @@ import ListGroup from "react-bootstrap/ListGroup";
 import Badge from "react-bootstrap/Badge";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Rating from "../Components/Rating";
 import { Helmet } from "react-helmet-async";
 import LoadingBox from "../Components/LoadingBox";
@@ -171,6 +171,7 @@ const reducer = (state, action) => {
 };
 
 function ProductScreen() {
+  const navigate = useNavigate();
   const params = useParams();
   const { slug } = params;
   const [{ loading, error, product }, dispatch] = useReducer(reducer, {
@@ -206,6 +207,7 @@ function ProductScreen() {
       type: 'CART_ADD_ITEM',
       payload: { ...product, quantity},
     });
+    navigate('/cart');
   };
 
   return loading ? (<LoadingBox />
