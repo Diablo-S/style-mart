cc74123a53dfadff1409dd3fd381babc29a9e1e0
Diablo-S <shreyashnegi123@gmail.com>
2022-12-03 12:02:09 +0530

    2nd commit

--- Stylemart/frontend/src/App.js
+++ Stylemart/frontend/src/App.js
@@ -1,12 +1,28 @@
+import data from "./data"
 
 function App() {
   return (
     <div>
       <header>
-        <a href="/">StyleMart</a>        
+        <a href="/">StyleMart</a>
       </header>
       <main>
-        List Of Products
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p><strong>Rs {product.price}</strong></p>
+                <button>Add to cart</button>
+              </div>
+            </div>))}
+        </div>
       </main>
     </div>
   );
