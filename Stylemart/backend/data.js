609932a4b0160bd5b764e5be6c85377daab7fa54
Diablo-S <shreyashnegi123@gmail.com>
2022-12-04 13:29:59 +0530

    15th commit

--- Stylemart/backend/data.js
+++ Stylemart/backend/data.js
@@ -1,4 +1,21 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+            name: 'Shreyash',
+            email: 'admin@example.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: true,
+        },
+        {
+            name: 'Ayush',
+            email: 'user@example.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: false,
+        },
+    ],
+
     products: [
         {
             // _id: '1',
@@ -129,7 +146,7 @@ const data = {
             rating: 4.8,
             numReviews: 25,
             description: 'Comfortable sports shorts',
-        },  
+        },
     ],
 };
 
